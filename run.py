03dd71a5a63748ded07608442e22ca2c22c7c28c
Anne Voretzsch <ac.voretzsch@gmx.de>
2024-10-03 19:52:37 +0000

    Create function to get blood pressure data via user input and set up main call function

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Values of variables do not change so they are constant variables written in capital letters
+# Values of variables do not change so they 
+# are constant variables written in capital letters
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,10 +17,37 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("pressure_watch")
-# Opens the exel sheet in google account that was set up before, name needs to be exact
+# Opens the exel sheet in google account that was set up before, 
+# name needs to be exact
 
-pressure = SHEET.worksheet("pressure")
+def get_pressure_data():
+    """
+    Get the blood pressure numbers (systolic and diastolic) 
+    via input from the user.
+    """
+    print("Please enter your systolic (upper number) and diastolic (lower number) blood pressure numbers for the last seven days.")
+    print("Numbers should be separated by commas.")
+    print("Example: 110, 115, 105, 98, 113, 99, 102\n")
 
-data = pressure.get_all_values()
+    data_str_one = input("Enter your systolic numbers here:\n")
 
-print(data)
+    pressure_systolic_data = data_str_one.split(",")
+
+    print(f"The numbers you entered are: {pressure_systolic_data}")
+
+    data_str_two = input("Enter your diastolic numbers here:\n")
+
+    pressure_diastolic_data = data_str_two.split(",")
+
+    print(f"The numbers you entered are: {pressure_diastolic_data}")
+
+
+
+        
+def main():
+    """
+    Run all program functions
+    """
+    data = get_pressure_data()
+
+main()
